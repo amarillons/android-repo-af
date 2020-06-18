439a2a1fa92d00969b3840d2d6bb8e7e787a0f42
a.f <akihiko.k.fujii@gmail.com>
2020-06-18 18:11:49 +0900

    Create screen

--- app/src/main/java/jp/afb114714/android/app/MainActivity.kt
+++ app/src/main/java/jp/afb114714/android/app/MainActivity.kt
@@ -2,11 +2,20 @@ package jp.afb114714.android.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "なんでも一覧表"
+
+        nextScreenButton.setOnClickListener({
+            Log.d("777","clicked")
+        })
     }
 }
+
